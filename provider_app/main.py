425fbcfb0714af84c12d55ce091fca1f1ce670ee
jacob <jacoborellana97@hotmail.com>
2018-04-19 10:08:47 -0500

    started credential check in provider app

--- provider_app/main.py
+++ provider_app/main.py
@@ -3,12 +3,41 @@ from kivy.properties import NumericProperty, StringProperty
 from sqlalchemy.exc import SQLAlchemyError
 from sys import stderr
 
+from kivy.logger import Logger
+
+from openmrs import RESTConnection
+from json import dumps
+
 # noinspection PyUnresolvedReferences
 from login import LoginScreen
 
 
 class ProviderApp(App):
-    pass
+    def __init__(self, **kwargs):
+        super(ProviderApp, self).__init__(**kwargs)
+        self.openmrs_connection = None
+
+    def user_login(self, username, password):
+        self.openmrs_connection = RESTConnection('localhost', 8080, username, password)
+        self.openmrs_connection.send_request()
+
+    def on_login_success(self, _, response):
+        print(dumps(response, indent=4, sort_keys=True))
+
+    def on_login_failure(self, _, error):
+        pass
+
+
+    def load_openmrs_data(self):
+        self.openmrs_connection.send_request('patient', None, self.on_openmrs_data_loaded,
+                                             self.on_openmrs_data_not_loaded,
+                                             self.on_openmrs_data_not_loaded(), '')
+
+    def on_openmrs_data_loaded(self):
+        pass
+
+    def on_openmrs_data_not_loaded(self, _, error):
+        Logger.error('RestApp: {error}'.format(error=error))
 
 
 if __name__ == '__main__':
