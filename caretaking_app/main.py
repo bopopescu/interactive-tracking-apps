616eb67fba67d34aab01fe70798cb09f969f484b
sam neupane <sam.neupane@huskers.unl.edu>
2018-04-21 13:58:23 -0500

    pulling changes of caretaking_app review screen modifications

--- caretaking_app/main.py
+++ caretaking_app/main.py
@@ -90,9 +90,9 @@ class CareTakingApp(App):
         self.account_patient_id = self.root.ids.create_account.ids.patient_id.text
         self.username = ('{g} {p}'.format(g=self.root.ids.create_account.ids.given_name.text, p = self.root.ids.create_account.ids.patient_id.text))
         self.verification = self.root.ids.create_account.ids.account_verification.text
-        if(self.account_surname == '' or self.account_given_name == '' or self.account_patient_id == ''):
-            self.verfication = 'You are missing one or many fields'
-        
+        self.missing_field = 'You are missing one or many fields'
+        if self.account_surname == '':
+            self.verification = self.missing_field
         else:
             self.root.transition.direction = 'left'
             self.root.current = 'login'
