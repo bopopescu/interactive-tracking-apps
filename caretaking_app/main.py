94b9fc2021eb9f42b87edcf607973349ad672fbb
Hassan Bakry Mousa Abdelsamad <hassanbakry2010@gmail.com>
2018-04-22 23:26:33 -0500

    pulling

--- caretaking_app/main.py
+++ caretaking_app/main.py
@@ -1,218 +1,122 @@
-from kivy.app import App
-from kivy.properties import StringProperty
-from kivy.uix.label import Label
-from sqlalchemy.exc import SQLAlchemyError
-from sys import stderr
+from sqlalchemy import create_engine, Column, Integer, String,  ForeignKey, DateTime, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-# noinspection PyUnresolvedReferences
-from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
-from sqlalchemy.sql.functions import user
-
-from database import Patient, User, Observation, CombinedDatabase
-from login_screen import LoginScreen
-# noinspection PyUnresolvedReferences
-from input_screen import ObservationEntry
-# noinspection PyUnresolvedReferences
-from create_account import CreateAccount
-# noinspection PyUnresolvedReferences
-from caretaking_review_screen import ReviewScreen
-# noinspection PyUnresolvedReferences
-from kivy.storage.jsonstore import JsonStore
+Persisted = declarative_base()
 
 
-class CareTakingApp(App):
-
-    store = JsonStore('users.json')
-    location = StringProperty('location type')
-    activity = StringProperty('activity')
-    appetite = StringProperty('appetite')
-    date = StringProperty('date')
-    city = StringProperty('city')
-    weight = StringProperty('weight')
-    temp = StringProperty('temperature')
-    patient = StringProperty('Select the patient visited')
-    error = StringProperty('')
-    patient_id = StringProperty('')
-    birth = StringProperty('')
-    account_surname = StringProperty('')
-    account_given_name = StringProperty('')
-    account_patient_id = StringProperty('')
-    missing_field = StringProperty('')
-    username = StringProperty('')
-    verification = StringProperty('')
-    failed = StringProperty('')
-    account_verification = StringProperty('')
-
-    def __init__(self, **kwargs):
-        super(CareTakingApp, self).__init__(**kwargs)
-        url = CombinedDatabase.construct_mysql_url('mysql.poetical-science.org', 3306, 'soft161_team_6', 'soft161_team_6', 'chromosome+differentiates<')
-        self.care_tracking_database = CombinedDatabase(url)
-        self.session = self.care_tracking_database.create_session()
-
-    def load(self):
-        self.load_kv('caretaking.kv')
-
-    #create log checks if the user has inputed vaild values and verifies their confirmation
-    def create_log(self):
-        self.patient_id = self.root.ids.observation.ids.patient_spinner.text
-        self.location = self.root.ids.observation.ids.location_type_spinner.text
-        self.activity = self.root.ids.observation.ids.physical_activity.text
-        self.appetite = self.root.ids.observation.ids.appetite.text
-        self.error = self.root.ids.observation.ids.submit.text
-        self.birth = self.root.ids.observation.ids.birthdate.text
-        self.city = self.root.ids.observation.ids.address.text
-        self.temp = self.root.ids.observation.ids.temp.text
-        self.weight = self.root.ids.observation.ids.weight.text
-        self.missing_field = 'You are missing one or many fields'
-        self.user_id = '10002T'
-
-        if self.patient_id == '':
-            self.error = self.missing_field
-        if self.location == 'Select the location type of visit':
-            self.error = self.missing_field
-        if self.activity == "Select value of Patient\'s physical activity":
-            self.error = self.missing_field
-        if self.appetite == "Select value of Patient\'s appetite level":
-            self.error = self.missing_field
-        if self.birth == '':
-            self.error = self.missing_field
-        if self.city == '':
-            self.error = self.missing_field
-        if self.temp == '':
-            self.error = self.missing_field
-        if self.weight == '':
-            self.error = self.missing_field
-        else:
-            self.review_screen()
-            self.error = 'Log Completed'
-            self.root.transition.direction = 'left'
-            self.root.current = 'review'
-
-        self.get_data()
-
-    def main_menu(self):
-        self.root.transition.direction = 'left'
-        self.root.current = 'login'
-
-    def login_in(self):
-<<<<<<< HEAD
-        self.username = ('{g} {p}'.format(g=self.root.ids.create_account.ids.given_name.text, p = self.root.ids.create_account.ids.patient_id.text))
-        self.account_verification = self.root.ids.login.ids.account_verification.text
-        if self.root.ids.login.ids.accounts.text == "Select your account":
-            self.account_verification = 'You must select an account to login or create an account'
-        else:
-=======
-         self.username = ('{g} {p}'.format(g=self.root.ids.create_account.ids.given_name.text, p = self.root.ids.create_account.ids.patient_id.text))
-         self.account_verification = self.root.ids.login.ids.account_verification.text
-         if self.root.ids.login.ids.accounts.text == "Select your account":
-             self.account_verification = 'You must select an account to login or create an account'
-         else:
->>>>>>> 7e92bbc8e9a4f681500d4562ca626fab450f3a90
-            self.root.transition.direction = 'left'
-            self.root.current = 'observation'
-
-    def create_account(self):
-        self.root.transition.direction = 'left'
-        self.root.current = 'create account'
-
-    # back to login makes sure when the user creates an account that they have inputted
-    # valid information
-    def back_to_login(self):
-        self.account_surname = self.root.ids.create_account.ids.surname.text
-        self.account_given_name = self.root.ids.create_account.ids.given_name.text
-        self.account_patient_id = self.root.ids.create_account.ids.patient_id.text
-        self.username = ('{g} {p}'.format(g=self.root.ids.create_account.ids.given_name.text, p = self.root.ids.create_account.ids.patient_id.text))
-        self.verification = self.root.ids.create_account.ids.account_verification.text
-        self.missing_field = 'You are missing one or many fields'
-        if self.account_surname == '' or self.account_given_name == '' or self.account_patient_id == '':
-            self.verification = self.missing_field
-        else:
-            self.root.transition.direction = 'left'
-            self.root.current = 'login'
-
-    # submit entry queries each observation, patient and user into the database
-    def _submit_entry(self):
-        user = User(surname = self.root.ids.observation.ids.patient_spinner.text, given_name=self.root.ids.observation.ids.patient_spinner.text)
-        patient = Patient(name = self.root.ids.observation.ids.patient_spinner.text, user_id = user.user_id)
-        observation = Observation(location = self.root.ids.observation.ids.location_type_spinner.text,\
-                                  activity = self.root.ids.observation.ids.physical_activity.text,\
-                                  appetite = self.root.ids.observation.ids.appetite.text,\
-                                  birth_date = self.root.ids.observation.ids.birthdate.text,\
-                                  city = self.root.ids.observation.ids.address.text,\
-                                  temperature = self.root.ids.observation.ids.temp.text,\
-                                  weight = self.root.ids.observation.ids.address.text,\
-                                  patient = patient)
-        try:
-            self.session.add(user)
-            self.session.add(patient)
-            self.session.add(observation)
-            self.session.commit()
-            print('Successful')
-        except SQLAlchemyError as exception:
-            print('Database setup failed!', file=stderr)
-            print('Cause: {exception}'.format(exception=exception), file=stderr)
-        except MultipleResultsFound:
-            print('Can not create, multiple results found')
-        except NoResultFound:
-            print('No results found')
-
-    def get_data(self):
-        jon = self.session.query(Patient).filter(Patient.name == 'Jon Smith').one()
-        jon.user_id = '10001V'
-        self.session.add(jon)
-        self.session.commit()
-        patients = self.session.query(Patient).filter(Patient.user_id == self.user_id).all()
-
-    def _load_state(self):
-        try:
-            users = []
-            self.root.ids.login.ids.accounts.values = list(self.store.keys())
-
-            print(list(self.store.keys()))
-        except KeyError:
-            pass
-
-    def _save_state(self):
-        self.store.put(str(self.account_patient_id), given_name = self.account_given_name)
-
-    def on_start(self):
-        self._load_state()
-
-    def on_pause(self):
-        self._save_state()
-        return True
-
-    def on_stop(self):
-        self._save_state()
-
-    def review_screen(self):
-        container = self.root.ids.review.ids.patient_records
-
-        #user_list = list(self.store.keys())
-        #patient_list = []
-        # for user in user_list:
-        #     patient_list.append(self.session.query(Patient).filter(Patient.user_id == user).all())
-
-        for observation in self.session.query(Observation).all():
-            container.add_widget(Label(text = observation.city))
+class CareLog(Persisted):
+    __tablename__ = 'logs'
+    care_log_id = Column(Integer, primary_key=True)
+    patients = relationship('Patient', uselist=True, secondary='observations')
+    observations = relationship('Observation', uselist=True, back_populates='log')
 
 
+class Observation(Persisted):
+    __tablename__ = 'observations'
+    observation_id = Column(Integer, primary_key=True)
+    patient_id = Column(Integer, ForeignKey('patients.patient_id'))
+    care_log_id = Column(Integer, ForeignKey('logs.care_log_id'))
+    location = Column(String(256))
+    activity = Column(String(256))
+    appetite = Column(String(256))
+    birth_date = Column(String(256))
+    city = Column(String(256))
+    weight = Column(String(256))
+    temperature = Column(String(256))
+    date_time = Column(DateTime)
+    log = relationship('CareLog', back_populates='observations')
+    patient = relationship('Patient', back_populates='observations')
 
 
+class PainEntry(Persisted):
+    __tablename__ = 'pain_entry'
+    pain_id = Column(Integer, primary_key=True)
+    time_stamp = Column(Date, nullable=False)
+    locations = relationship('PainLocation', uselist=True, secondary='pain_entry_location')
+    pain_entry_location = relationship('PainEntryLocation', uselist=True, back_populates='pain_entry')
 
 
+class PainLocation(Persisted):
+    __tablename__ = 'locations'
+    location_id = Column(Integer, primary_key=True)
+    body_location = Column(String(256), nullable=False)
+    pain_entries = relationship('PainEntry', uselist=True, secondary='pain_entry_location')
+    pain_entry_location = relationship('PainEntryLocation', uselist=True, back_populates='pain_location')
 
 
+class PainEntryLocation(Persisted):
+    __tablename__ = 'pain_entry_location'
+    pain_id = Column(Integer, ForeignKey('pain_entry.pain_id'), primary_key=True)
+    location_id = Column(Integer, ForeignKey('locations.location_id'), primary_key=True)
+    severity = Column(Integer)
+    pain_entry = relationship('PainEntry', back_populates='pain_entry_location')
+    pain_location = relationship('PainLocation', back_populates='pain_entry_location')
 
 
+class MedicationEntry(Persisted):
+    __tablename__ = 'medication_entry'
+    medication_id = Column(Integer, primary_key=True)
+    time_stamp = Column(Date, nullable=False)
+    medicines = relationship('Medicine', uselist=True, secondary='medication_entry_dosage')
+    medication_dosage = relationship('MedicationEntryDosage', uselist=True, back_populates='medication_entry')
 
 
+class Medicine(Persisted):
+    __tablename__ = 'medicine'
+    medicine_id = Column(Integer, primary_key=True)
+    medicine_type = Column(String(256), nullable=False)
+    medication_entries = relationship('MedicationEntry', uselist=True, secondary='medication_entry_dosage')
+    medication_dosage = relationship('MedicationEntryDosage', uselist=True, back_populates='medicine')
 
-if __name__ == '__main__':
-    try:
-        app = CareTakingApp()
-        app.run()
-    except SQLAlchemyError as exception:
-        print('Initial database connection failed!', file=stderr)
-        print('Cause: {exception}'.format(exception=exception), file=stderr)
-        exit(1)
+
+class MedicationEntryDosage(Persisted):
+    __tablename__ = 'medication_entry_dosage'
+    medication_id = Column(Integer, ForeignKey('medication_entry.medication_id'), primary_key=True)
+    medicine_id = Column(Integer, ForeignKey('medicine.medicine_id'), primary_key=True)
+    dosage = Column(Integer)
+    medicine = relationship('Medicine', back_populates='medication_dosage')
+    medication_entry = relationship('MedicationEntry', back_populates='medication_dosage')
+
+
+class User(Persisted):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True)
+    open_mrs_ID = Column(String(256))
+    surname = Column(String(256), nullable=False)
+    given_name = Column(String(256), nullable=False)
+    patient = relationship('Patient', back_populates='user')
+
+
+class Patient(Persisted):
+    __tablename__ = 'patients'
+    patient_id = Column(Integer, primary_key=True)
+    name = Column(String(256), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.user_id'))
+    name = Column(String(256))
+    open_mrs_id = Column(String(256), ForeignKey('users.user_id'))
+    user = relationship('User', back_populates='patient')
+    observations = relationship('Observation', uselist=True, back_populates='patient')
+
+
+class CombinedDatabase(object):
+    @staticmethod
+    def construct_mysql_url(authority, port, database, username, password):
+        return 'mysql+mysqlconnector://{username}:{password}@{authority}:{port}/{database}' \
+            .format(authority=authority, port=port, database=database, username=username, password=password)
+
+    @staticmethod
+    def construct_in_memory_url():
+        return 'sqlite:///'
+
+    def __init__(self, url):
+        self.engine = create_engine(url)
+        self.Session = sessionmaker()
+        self.Session.configure(bind=self.engine)
+
+    def ensure_tables_exist(self):
+        Persisted.metadata.create_all(self.engine)
+
+    def create_session(self):
+        return self.Session()
